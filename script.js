9de9e5caa5e61eca060532d4dcaeaf9db4e81f81
nereikia-linas <linas.nereikia@gmail.com>
2021-06-05 13:55:23 +0300

    įdėjau į funkciją!

--- script.js
+++ script.js
@@ -5,7 +5,14 @@ var area = [2.1, 9596961, 9984670, 357022, 65300, 92212];
 var people = [38.300, 1444390177, 38048738, 83166711, 2793694, 10260009];
 var million = 1000000;
 var i;
-for (i = 0; i < countries.length; i++) {
-  console.log('Šalis: ' + countries[i] + ', joje gyvena ' + (people[i] / million).toFixed(2) + ' mln. gyventojų. Valstybės plotas: '
-                 + area[i] + ' km², plotas tenkantis vienam gyventojui: ' + ((area[i] * million) / people[i]).toFixed(2) + ' m²');
+
+function pullInfo() {
+  for (i = 0; i < countries.length; i++) {
+    console.log(countries[i] + ', gyventojų ' + (people[i] / million).toFixed(2) + ' mln. Šalies plotas: '
+    + area[i] + ' km², plotas tenkantis vienam gyventojui: ' + ((area[i] * million) / people[i]).toFixed(2) + ' m²');
+  }
 }
+
+pullInfo(
+  alert('Sužinokite kur erdviau gyventi — Kinijoje, Monake ar Kanadoje, o gal tai Vokietija, Lietuva ar Portugalija?')
+)
